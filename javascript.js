b1cf7d500b750103be9737b7330f4d64b213e46d
trener-107 <makssokolov107@gmail.com>
2021-06-18 12:48:34 +0300

    add file

---
+++ javascript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".icon_arrow").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
